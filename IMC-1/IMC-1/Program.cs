3b624df03966049d8070e1de68bb0e1c42623a80
Aloe Heros <heros.dasilva@gmail.com>
2022-07-14 21:04:58 -0300

    Exercício IMC

---
+++ IMC-1/IMC-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace imc_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu peso: ");
+            float peso = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite sua altura: ");
+            float altura = (float)Convert.ToDouble(Console.ReadLine());
+
+
+
+            float imc = peso / (altura * altura);
+
+
+        }
+    }
+}
